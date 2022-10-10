f20203c9630b5995b23c1415d5ff9c83143f65f1
Alhaadee <alhaadee91@gmail.com>
2022-10-10 10:43:04 +0100

    add post game function

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 const GameContainer = () => {
     const [players, setPlayers] = useState([]);
     const [player1Turn, setPlayer1Turn] = useState(true);
+    const [games, setGames] = useState([]);
     
     // post player
     const addPlayer = async (playerName) => {
@@ -16,10 +17,23 @@ const GameContainer = () => {
         setPlayers([...players, savedPlayer]);
     }
     // post request to start new game
-    // connect to game
-    // patch to update game status 
+  
 
-    const fetchGame
+    const postGame = async (playerId) => {
+        const response = await fetch("http://localhost:8080/games/start?player1Id=" + playerId, {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(playerId)
+        })
+        const savedGame = await response.json();
+        setGames([...games, savedGame]);
+    }
+
+      // connect to game
+
+
+    // patch to update game status 
+    
 
     return (
         <div className="main_container">
@@ -30,7 +44,7 @@ const GameContainer = () => {
 
 
     );
+    }
 
-}
 
 export default GameContainer;
