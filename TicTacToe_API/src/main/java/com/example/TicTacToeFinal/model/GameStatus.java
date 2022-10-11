923cc5885333fa69928cbc84c6413ee1749f73c4
Alhaadee <alhaadee91@gmail.com>
2022-10-11 10:00:05 +0100

    removed an api

--- TicTacToe_API/src/main/java/com/example/TicTacToeFinal/model/GameStatus.java
+++
@@ -1,8 +0,0 @@
-package com.example.TicTacToeFinal.model;
-
-public enum GameStatus {
-
-    NEW, ONGOING, FINISHED
-}
-
-//THIS IS UPDATED WITH GAME PLAY
--- TicTacToe_API/src/main/java/com/example/TicTacToeFinal/repositories/PlayerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.TicTacToeFinal.repositories;
-
-import com.example.TicTacToeFinal.model.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Player, Long> {
-}
