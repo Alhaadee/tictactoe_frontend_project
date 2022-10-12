983bf8effb489d9edf95ebe1e9828f3456e1b2b7
Faizan Khan <faizanahmkhan@gmail.com>
2022-10-12 16:18:46 +0100

    made grid responsive

--- src/components/Game.js
+++ src/components/Game.js
@@ -11,15 +11,15 @@ return (
         { game.winner === "O" ? <h2>{player2.name} wins</h2> : <div></div> }
     </div>
     <div className="board">
-        <Grid className = "top-left" makeMove = {makeMove} game = {game} player1Turn={player1Turn} />
-        <Grid className = "top-middle" makeMove = {makeMove} game = {game} player1Turn={player1Turn} />
-        <Grid className = "top-right" makeMove = {makeMove} game = {game} player1Turn={player1Turn} />
-        <Grid className = "middle-left" makeMove = {makeMove} game = {game} player1Turn={player1Turn} />
-        <Grid className = "middle-middle" makeMove = {makeMove} game = {game} player1Turn={player1Turn} />
-        <Grid className = "middle-right" makeMove = {makeMove} game = {game} player1Turn={player1Turn} />
-        <Grid className = "bottom-left" makeMove = {makeMove} game = {game} player1Turn={player1Turn} />
-        <Grid className = "bottom-middle" makeMove = {makeMove} game = {game} player1Turn={player1Turn} />
-        <Grid className = "bottom-right" makeMove = {makeMove} game = {game} player1Turn={player1Turn} />
+        <div className = "top-left"><Grid className = "top-left" makeMove = {makeMove} game = {game} player1Turn={player1Turn} /></div>
+        <div className = "top-middle"><Grid className = "top-middle" makeMove = {makeMove} game = {game} player1Turn={player1Turn} /></div>
+        <div className = "top-right"><Grid className = "top-right" makeMove = {makeMove} game = {game} player1Turn={player1Turn} /></div>
+        <div className = "middle-left"><Grid className = "middle-left" makeMove = {makeMove} game = {game} player1Turn={player1Turn} /></div>
+        <div className = "middle-middle"><Grid className = "middle-middle" makeMove = {makeMove} game = {game} player1Turn={player1Turn} /></div>
+        <div className = "middle-right"><Grid className = "middle-right" makeMove = {makeMove} game = {game} player1Turn={player1Turn} /></div>
+        <div className = "bottom-left"><Grid className = "bottom-left" makeMove = {makeMove} game = {game} player1Turn={player1Turn} /></div>
+        <div className = "bottom-middle"><Grid className = "bottom-middle" makeMove = {makeMove} game = {game} player1Turn={player1Turn} /></div>
+        <div className = "bottom-right"><Grid className = "bottom-right" makeMove = {makeMove} game = {game} player1Turn={player1Turn} /></div>
        
 
     </div>
